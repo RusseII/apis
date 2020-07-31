8e07768a8d18a40adf4643d48e793fe0b7e4b6f6
russell <russell@deephire.com>
2020-07-31 11:36:23 -0500

    format json

--- api/tg.ts
+++ api/tg.ts
@@ -5,7 +5,10 @@ const url = `https://api.telegram.org/bot${process.env.TG_API_KEY}/sendMessage`
 
 const chat_id = "-372756431"
 export default async (req: NowRequest, res: NowResponse) => {
-  const data = { chat_id, text: req.body}
+ const bodyObj = JSON.parse(req.body)
+  const pretty =  JSON.stringify(bodyObj,null,2)
+  const data = { parse_mode: 'MarkdownV2', chat_id, text: `\`\`\`json \n${pretty}\`\`\``}
+  console.log(data)
   const r = await fetch(url, { method: "POST", headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(data) })
   
   res.json({status: r.status})
